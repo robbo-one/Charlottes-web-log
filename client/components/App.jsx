53a8958709b13e1b6014c7d4949efd0e7770a79d
jeff-seymour <73873192+jeff-seymour@users.noreply.github.com>
2021-01-25 12:28:02 +1300

    posts load up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import Posts from './Posts.jsx'
 
 // importing data
 import headerData from '../../data/header'
 import footerData from '../../data/footer'
+// import postsData from '../../data/posts'
 
 
 function App(props) {
@@ -12,7 +14,7 @@ function App(props) {
         <div className="container">
             <Header className="header" title={headerData.title} link={headerData.link} />
             <div className='content'>
-
+                <Posts className='posts'/>
             </div>
             <Footer className="footer" author={footerData.author} copyright={footerData.copyright} />
         </div>
---
+++ client/components/Posts.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Post from '../../data/posts'
+
+function Posts (props){
+    return(
+        <div>
+            <h1>Posts</h1>
+            <div>
+                {Post.map(aPost => {
+                    return (
+                        <div>
+                            <h3>{aPost.title}</h3>
+                            <h4>{aPost.date}{aPost.commentCount}</h4>
+                            {aPost.paragraphs.map(para =>{
+                                return (
+                                    <div>
+                                        <p>{para}</p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Posts
