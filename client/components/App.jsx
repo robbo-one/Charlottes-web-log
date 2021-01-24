8215313c1bffb2fbab6fe689d8001e64a5bafcb4
Tom May <73091215+tom-may@users.noreply.github.com>
2021-01-25 12:01:10 +1300

    add footer

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import headerData from '../../data/header'
+import Footer from './Footer'
+import footerData from '../../data/footer'
 
-export default function App (props) {
+export default function App(props) {
   return (
-    <Header title={headerData.title} link={headerData.link} />
+    <div>
+      <Header title={headerData.title} link={headerData.link} />
+      <Footer copyright={footerData.copyright} author={footerData.author} />
+    </div>
   )
 }
---
+++ client/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer (props) {
+  return (
+    <footer> {props.copyright}{props.author} </footer>
+  )
+}
