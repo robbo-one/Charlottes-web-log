01c010e05da509748b4aa12689fbefc49e198157
student <student@eda-bc-11.localdomain>
2021-01-25 17:51:39 +1300

    Add header

---
+++ client/Components/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from './Header' 
+// import Footer from './Footer'
+// import Posts from './Posts'
+
+import headerData from '../../data/header'
+// import footerData from '../../data/footer'
+// import posts from '../../data/posts'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Header name={headerData.title} link={headerData.link}/>
+    
+    </div>
+  )
+}
+
+export default App
+
+
+  {/* <Posts posts={posts}/>
+      <Footer copyright={footerData.copyright} author={footerData.author}/> */}
---
+++ client/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <div className='header-Wrapper'>
+      <span className='header-name'>{props.name}</span>
+      <span className='header-link'>{props.link}</span>
+    </div>
+  )
+}
+export default Header
+
+
