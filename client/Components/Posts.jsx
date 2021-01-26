d7e4596bb4300ef0b9bb2340b393750d7a3f3518
student <student@eda-bc-02.localdomain>
2021-01-26 20:00:21 +1300

    Playing with CSS

--- client/Components/Posts.jsx
+++ client/Components/Posts.jsx
@@ -1,3 +1,10 @@
+//This goes through each post in the posts.js data array (which contains id, /////title, date, comment count and paragraphs.  
+//It allows the post id to be associated with a particular post.  
+//The Post.jsc component receives a JS object const proprs = thing.title, thing.data, thing.para.  
+//This maps and returns the JS object??
+//Post is inside posts.  Post exports to Posts 
+
+
 import React from 'react'
 
 import Post from './Post'
